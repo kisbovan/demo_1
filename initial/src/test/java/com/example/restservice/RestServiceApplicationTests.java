11e4b376bb7f4b3d113e157e961d3130c3b48f77
kisbovan <kisbovan@gmail.com>
2021-01-19 19:36:59 +0100

    removed some imports

--- initial/src/test/java/com/example/restservice/RestServiceApplicationTests.java
+++ initial/src/test/java/com/example/restservice/RestServiceApplicationTests.java
@@ -2,7 +2,6 @@ package com.example.restservice;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -14,7 +13,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.http.MediaType;
-import org.springframework.http.HttpHeaders;
 
 import static org.hamcrest.Matchers.*;
 import org.json.JSONObject;
